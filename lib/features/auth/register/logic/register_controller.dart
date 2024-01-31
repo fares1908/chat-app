ffa06df6544babcf641d90dd1352954ba157a2c7
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2024-01-31 08:20:58 +0200

    complete register_logic

--- lib/features/auth/register/logic/register_controller.dart
+++ lib/features/auth/register/logic/register_controller.dart
@@ -4,10 +4,12 @@ import '../../../../core/class/status_request.dart';
 import '../../../../core/helpers/functions/handling_data.dart';
 import '../../../../core/routing/routes.dart';
 import '../data/register_data.dart';
+
 abstract class RegisterController extends GetxController {
   goToRegister();
   goToLogin();
 }
+
 class RegisterControllerImpl extends RegisterController {
   late TextEditingController email;
   late TextEditingController password;
@@ -20,49 +22,31 @@ class RegisterControllerImpl extends RegisterController {
   goToLogin() {
     Get.offNamed(AppRouter.loginScreen);
   }
+
   @override
   goToRegister() async {
     if (formState.currentState!.validate()) {
       statusRequest = StatusRequest.loading;
       update();
       try {
-        // Make the registration request
         var response = await registerData.registerData(
             email.text, password.text, firstname.text, lastname.text);
-
-        // Print the response for debugging purposes
         print("=============================== Controller $response");
-
-        // Handle the response using the handlingData function
         statusRequest = handlingData(response);
-
-        // Check if the registration was successful
         if (StatusRequest.success == statusRequest) {
           // Check if the status in the response is 'success'
           if (response['status'] == 'success') {
             goToLogin();
-            // Registration success logic
+
             print("=============================== $statusRequest");
           } else {
-            // Check if the error is due to an existing user
-            if (response['status'] == 'fail' &&
-                response['message'] == 'User already exists') {
-              // Show a dialog or notification indicating that the user already exists
-              await Get.defaultDialog(
-                title: "Warning",
-                middleText: "User already exists",
-              );
-            } else {
-              // Handle other types of errors if needed
-              Get.snackbar('Error', 'Registration failed. Please try again.');
-            }
-
-            // Update the statusRequest to indicate failure
             statusRequest = StatusRequest.failure;
           }
+        } else {
+          Get.snackbar('Warning', 'Email already exists');
+          print('Error');
         }
 
-        // Update the UI if needed
         update();
       } catch (e) {
         // Handle any exceptions that may occur during the registration process
@@ -76,8 +60,6 @@ class RegisterControllerImpl extends RegisterController {
       Get.snackbar('Error', 'Registration failed. Please try again.');
     }
   }
-
-
 
   @override
   @override
